a558222ef4698e19af6894c62a30e3f327781f14
janzheng <janeazy@gmail.com>
2021-07-02 09:11:00 -0400

    added sharing link

--- components/dir/PeopleCard.vue
+++ components/dir/PeopleCard.vue
@@ -119,6 +119,9 @@
           </div>
         </div>
 
+        <div class="People-share _font-small _margin-top-2">
+          Share: <a :href="`https://phage.directory/people#${person.fields['Slug']}`" >https://phage.directory/people#{{person.fields['Slug'] }}</a>
+        </div>
       </div>
     
     </div>

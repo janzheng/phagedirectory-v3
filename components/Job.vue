afba79643e07846f1d5cea38ffb5fe974b3a02d3
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2022-12-15 16:21:16 +1100

    bump

--- components/Job.vue
+++ components/Job.vue
@@ -47,11 +47,11 @@
     </div>
 
     <div v-if="!isExpired && atom.fields['URL']" class="Job-action _margin-top">
-      <a v-if="atom.fields['Data:CTA']" :href="atom.fields['Data:CTA']" class="Job-action-apply CTA _button --short --bright _margin-bottom-none-i _margin-right-half" target="_blank">Apply Here</a>
-      <a v-if="atom.fields['Slug']" :href="`/jobs/${atom.fields['Slug']}`" class="Job-action-apply _button --short --outline _margin-bottom-none-i _margin-right-half" >Read more about the job</a>
-      <span v-if="atom.fields['Data:DateEnd']" class="Job-expiry _font-small --nowrap">
+      <a v-if="atom.fields['URL']" :href="atom.fields['URL']" class="Job-action-apply CTA --short --outline _margin-bottom-none-i _margin-right-half" target="_blank">Apply Here</a>
+      <!-- <a v-else-if="atom.fields['Slug']" :href="`/jobs/${atom.fields['Slug']}`" class="Job-action-apply _button --short --outline _margin-bottom-none-i _margin-right-half" >Read more about the job</a> -->
+      <!-- <span v-if="atom.fields['Data:DateEnd']" class="Job-expiry _font-small --nowrap">
         Last day to apply: <span class="_font-bold">{{ atom.fields['Data:DateEnd'] | niceDate }} </span>
-      </span>
+      </span> -->
     </div>
     <div v-else>
       <span v-if="atom.fields['URL']" class="Job-action-apply _button --short --disabled --outline _margin-bottom-none _margin-right-half" >Job expired</span>
